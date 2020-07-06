d42e1d11507dd56abb8f20fdbb307ae00a11e439
abir <abirchandrabhushan@gmail.com>
2020-07-07 01:09:13 +0530

    add more tests

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ class WDYM extends Transform {
   /**
    * transforms input stream in Common Log Format into useful JSON
    * @param {Object} chunk - the input stream of data
-   * @param {String} encoding - data encoding
-   * @param {Function} callback
+   * @param {String} encoding - character encoding of the chunk
+   * @param {Function} callback - called when processing is complete for the supplied chunk
    */
   _transform(chunk, encoding, callback) {
     const input = chunk.toString()
     input.split(/\n/).forEach((line) => {
       const re = /([^ ]*) ([^ ]*) ([^ ]*) \[([^\]]*)\] "([^"]*)" ([^ ]*) ([^ ]*)/
       const matches = line.match(re)
+      console.log(matches)
       if (matches) {
         const log = {
           remoteHost: matches[1],
@@ -22,7 +23,7 @@ class WDYM extends Transform {
           date: new Date(matches[4]),
           request: matches[5],
           status: Number(matches[6]),
-          bytes: Number(matches[7]) || 0,
+          size: Number(matches[7]) || 0,
         }
         this.push(JSON.stringify(log))
       }
--- test/pipe.js
+++ test/pipe.js
@@ -3,24 +3,88 @@ const wdym = require('../')
 
 describe('CLF Log', () => {
   it('should convert to JSON without error', () => {
-    let literals = undefined
+    let json = undefined
     const transform = new wdym()
     transform.on('readable', function () {
       while ((data = this.read())) {
-        literals = JSON.parse(data)
+        json = JSON.parse(data)
       }
-      literals.should.be.eql({
+      json.should.be.eql({
         remoteHost: '127.0.0.1',
         remoteLogName: '-',
         authUser: '-',
         date: '2020-07-07T16:42:00.000Z',
         request: 'GET /index.html HTTP/1.1',
         status: 200,
-        bytes: 733,
+        size: 733,
       })
     })
     transform.write(
-      '127.0.0.1 - - [Wed, 07 July 2020 16:42:00 GMT] "GET /index.html HTTP/1.1" 200 733 "http://localhost:8000/" "userAgent"'
+      '127.0.0.1 - - [Wed, 07 July 2020 16:42:00 GMT] "GET /index.html HTTP/1.1" 200'
+    )
+  })
+})
+
+describe('CLF Log with non-numeric bytes', () => {
+  it('should convert to JSON without error and change the size of the object to 0', () => {
+    let json = undefined
+    const transform = new wdym()
+    transform.on('readable', function () {
+      while ((data = this.read())) {
+        json = JSON.parse(data)
+      }
+      json.should.be.eql({
+        remoteHost: '127.0.0.1',
+        remoteLogName: '-',
+        authUser: '-',
+        date: '2020-07-07T16:42:00.000Z',
+        request: 'GET /index.html HTTP/1.1',
+        status: 200,
+        size: 0,
+      })
+    })
+    transform.write(
+      '127.0.0.1 - - [Wed, 07 July 2020 16:42:00 GMT] "GET /index.html HTTP/1.1" 200'
+    )
+  })
+})
+
+describe('CLF Log with extra information', () => {
+  it('should convert to JSON without error and ignore the extra details', () => {
+    let json = undefined
+    const transform = new wdym()
+    transform.on('readable', function () {
+      while ((data = this.read())) {
+        json = JSON.parse(data)
+      }
+      json.should.be.eql({
+        remoteHost: '127.0.0.1',
+        remoteLogName: '-',
+        authUser: '-',
+        date: '2020-07-07T16:42:00.000Z',
+        request: 'GET /index.html HTTP/1.1',
+        status: 200,
+        size: 0,
+      })
+    })
+    transform.write(
+      '127.0.0.1 - - [Wed, 07 July 2020 16:42:00 GMT] "GET /index.html HTTP/1.1" 200 "http://localhost:8000" "userAgent"'
+    )
+  })
+})
+
+describe('CLF Log with extra information', () => {
+  it('should convert to JSON without error and ignore the extra details', () => {
+    let json = undefined
+    const transform = new wdym()
+    transform.on('readable', function () {
+      while ((data = this.read())) {
+        json = JSON.parse(data)
+      }
+      json.should.be.eql(undefined)
+    })
+    transform.write(
+      '2010-05-02 15:42:15 - 40.89.255.10  34.14.255.10 80 GET /default.htm 200 - HTTP/1.0 Mozilla/4.0  (compatible: MSIE+5.5+Windows+2000+Server)'
     )
   })
 })
