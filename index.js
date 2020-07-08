262e1be3e8035f32cf4b66ee071e882243980d3e
abir <abirchandrabhushan@gmail.com>
2020-07-08 23:54:16 +0530

    added functionality to convert to CSV

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const stream = require('stream')
 const { parse } = require('date-fns')
-const createCsvWriter = require('csv-writer').createObjectCsvStringifier
+const createCSVWriter = require('csv-writer').createObjectCsvWriter
+const createCSVStringifier = require('csv-writer').createObjectCsvStringifier
 
 const IncorrectFormatError = require('./custom/IncorrectFormatError')
 const messages = require('./custom/messages')
@@ -32,31 +33,6 @@ class WDYM extends stream.Transform {
     callback()
   }
 
-  toJSON(clf) {
-    let json = { log: [] }
-    clf.forEach((line) => {
-      const matches = this.isCLF(line)
-      if (matches) {
-        const serverLog = {
-          remoteHost: matches[1],
-          remoteLogName: matches[2],
-          authUser: matches[3],
-          date:
-            Date.parse(matches[4]) || this.parseDate(matches[4])
-              ? this.parseDate(matches[4])
-              : 'unreadable',
-          request: matches[5],
-          status: Number(matches[6]) || 'Invalid Status Code',
-          size: Number(matches[7]) || 0,
-        }
-        json.log.push(serverLog)
-      } else {
-        throw new IncorrectFormatError()
-      }
-    })
-    return json
-  }
-
   /**
    * Matches the line to the standardised (ASCII) CLF Log format.
    * https://www.iri.com/blog/migration/data-migration/clf-elf-web-log-formats/
@@ -70,10 +46,76 @@ class WDYM extends stream.Transform {
   }
 
   /**
+   * Parses the CLF logs and converts them to JSON.
+   * @param {Array} clf - the CLF Logs
+   * @throws {IncorrectFormatError}
+   * @returns {Object}
+   */
+
+  toJSON(clf) {
+    let json = { log: [] }
+    clf.forEach((line) => {
+      const matches = this.isCLF(line)
+      if (matches) {
+        const serverLog = {
+          remoteHost: matches[1],
+          remoteLogName: matches[2],
+          authUser: matches[3],
+          date:
+            Date.parse(matches[4]) || this._parseDate(matches[4])
+              ? this._parseDate(matches[4])
+              : 'unreadable',
+          request: matches[5],
+          status: Number(matches[6]) || 'Invalid Status Code',
+          size: Number(matches[7]) || 0,
+        }
+        json.log.push(serverLog)
+      } else {
+        throw new IncorrectFormatError()
+      }
+    })
+    return json
+  }
+
+  toCSV(serverLog) {
+    const schema = this._getCSVSchema(serverLog)
+    const csvStringifier = createCSVStringifier({ header: schema })
+    return csvStringifier
+      .getHeaderString()
+      .concat(csvStringifier.stringifyRecords([serverLog]))
+  }
+
+  get _csvWriter() {
+    const schema = this._getCSVSchema(serverLog)
+    return createCSVWriter({ path: './output.csv', header: schema })
+  }
+
+  _getCSVSchema(serverLog) {
+    const keys = Object.keys(serverLog)
+    const titles = [
+      'REMOTE HOST',
+      'REMOTE LOG NAME',
+      'USER ID',
+      'DATE',
+      'REQUEST',
+      'HTTP STATUS CODE',
+      'SIZE',
+    ]
+    let i = 0,
+      schema = []
+
+    while (i < keys.length) {
+      schema.push({ id: keys[i], title: titles[i] })
+      i += 1
+    }
+    return schema
+  }
+
+  /**
    * Parses CLF Standard date ([day/month/year:hour:minute:second zone])
    * @param {String} date
    */
-  parseDate(date) {
+  _parseDate(date) {
     if (Date.parse(date)) return new Date(date)
 
     const strftime = /([\w:/]+\s[+\-]\d{4})/
